c0170b6f64a9ae4c3a8a40bd656a0c260293f92f
Radega12 <rauldearriba@gmail.com>
2020-03-06 18:38:40 +0100

    end args

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,10 +1,40 @@
 //requires
-const { crearArchivo } = require('./multiplicar/multiplicar');
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+const argv = require('yargs')
+  .command('listar', 'Imprime en consola la tabla de multiplicar', {
+    base: {
+      demand: true,
+      alias: 'b'
+    },
+    limite: {
+      alias: 'l',
+      default: 10
+    }
+  })
+  .command('crear', 'Crea un fichero con la tabla de multiplicar', {
+    base: {
+      demand: true,
+      alias: 'b'
+    },
+    limite: {
+      alias: 'l',
+      default: 10
+    }
+  })
+  .help()
+  .argv;
 
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1];
+let comando = argv._[0];
 
-crearArchivo(base)
-  .then((archivo) => console.log(`El archivo tabla-${base}.txt a sido creado!`))
-  .catch((err) => console.log(err));
+switch (comando) {
+  case 'listar':
+    listarTabla(argv.base, argv.limite);
+    break;
+  case 'crear':
+    crearArchivo(argv.base,  argv.limite)
+      .then(archivo => console.log(`El archivo tabla-${argv.base}.txt a sido creado!`))
+      .catch(err => console.log(err));
+    break;
+  default:
+    console.log('Comando no reconocido');
+}
--- 03-bases-node/multiplicar/multiplicar.js
+++ 03-bases-node/multiplicar/multiplicar.js
@@ -1,21 +1,27 @@
 //requireds
 const fs = require('fs');
 
+let listarTabla = (base, limite = 10) => {
+  for (let i = 1; i <= limite; i++) {
+    console.log(`${base} * ${i} = ${base*i}`);
+  }
+}
 
-let crearArchivo = (base) => {
+let crearArchivo = (base, limite = 10) => {
   return new Promise(function(resolve, reject) {
     if (!Number(base)) {
       reject(`El valor introducido ${base} no es un numero`)
+      return;
     }
 
     let data = '';
-    for (let i = 1; i <= 10; i++) {
-      data += `${base} * ${i} = ${base*i}\n`;
+    for (let i = 1; i <= limite; i++) {
+      data += `${ base } * ${ i } = ${ base * i }\n`;
     }
 
     fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
 
-      if (err){
+      if (err) {
         reject(err);
       } else {
         resolve(`tabla-${base}.txt`);
@@ -27,5 +33,6 @@ let crearArchivo = (base) => {
 }
 
 module.exports = {
-  crearArchivo
+  crearArchivo,
+  listarTabla
 };
