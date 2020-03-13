96a2a714534042553fc2764640732e2f8e7d37be
Radega12 <rauldearriba@gmail.com>
2020-03-13 13:24:47 +0100

    end webpage module 06

---
+++ 06-webserver/README.md
@@ -0,0 +1,12 @@
+# Aplicación web Express - CursoNode
+
+
+Recuerden ejecutar
+```
+npm install
+```
+
+## Ejemplo
+```
+node server.js
+```
--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -3,6 +3,7 @@ const app = express()
 const hbs = require('hbs');
 require('./hbs/helpers')
 
+const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
 //Express hbs
@@ -20,6 +21,6 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
-app.listen(3000, () => {
-  console.log('Escuchando peticiones en el puerto 3000');
+app.listen(port, () => {
+  console.log(`Escuchando peticiones en el puerto ${port}`);
 })
