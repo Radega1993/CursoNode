a280cd4d99dc76bf21a9832c45653c2d3b95255e
Radega12 <rauldearriba@gmail.com>
2020-03-14 17:00:53 +0100

    post, put and get

--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -1,6 +1,9 @@
 require('./config/config');
 
+
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -13,40 +16,17 @@ app.use(bodyParser.urlencoded({
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-  res.json('get usuario');
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function(req, res) {
+mongoose.connect('mongodb://localhost:27017/cafe', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  },
+  (err, res) => {
+    if (err) throw err;
 
-  let body = req.body;
-
-  if (body.nombre == undefined) {
-    res.status(400).json({
-      ok: false,
-      mensaje: 'El nombre es necesario'
-    });
-  } else {
-    res.json({
-      persona: body
-    });
-  }
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-  let id = req.params.id;
-
-  res.json({
-    id
+    console.log('Base de datos ONLINE');
   });
-
-});
-
-app.delete('/usuario', function(req, res) {
-  res.json('delete usuario');
-});
 
 app.listen(process.env.PORT, () => {
   console.log('Escuchando puerto: ', process.env.PORT);
