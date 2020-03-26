997419ac0f93b3df601f922c154537a86e2cb185
Radega12 <rauldearriba@gmail.com>
2020-03-26 20:29:42 +0100

    final chat

--- 10-sockets-chat/server/sockets/socket.js
+++ 10-sockets-chat/server/sockets/socket.js
@@ -20,16 +20,18 @@ io.on('connection', (client) => {
     usuarios.agregarPersona(client.id, data.nombre, data.sala);
 
     client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala))
-
+    client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('Administrador', `${data.nombre} se unió`));
     callback(usuarios.getPersonasPorSala(data.sala));
   });
 
-  client.on('crearMensaje', (data) => {
+  client.on('crearMensaje', (data, callback) => {
 
     let persona = usuarios.getPersona(client.id);
 
     let mensaje = crearMensaje(persona.nombre, data.mensaje);
     client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+
+    callback(mensaje);
   });
 
   client.on('disconnect', () => {
